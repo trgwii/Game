0c083465f4e31430195a6705803e81ef307edd67
Thomas Rory Gummerson <thomas@gummerson.no>
2021-03-11 11:02:36 +0100

    init

---
+++ wrap.ts
@@ -0,0 +1,15 @@
+import { readLines } from "https://deno.land/std@0.90.0/io/bufio.ts";
+
+const proc = Deno.run({
+  env: {
+    PLUGIN_URL: "pane.dll",
+  },
+  cmd: ["deno", "run", "-A", "--unstable", "pane.ts"],
+  stderr: "piped",
+});
+for await (const line of readLines(proc.stderr)) {
+  console.error(line);
+  if (line === "exit") {
+    proc.kill(Deno.Signal.SIGKILL);
+  }
+}
